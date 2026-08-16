91a648fcc06c38c2440ecdb883583666fc9ee353
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2026-08-16 10:37:20 +0300

    validateId2

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,22 +1,5 @@
 import createHttpError from 'http-errors';
 
-// export const errorHandler = (err, req, res, next) => {
-//   if (err instanceof createHttpError) {
-//     res.status(err.status).json({
-//       status: err.status,
-//       message: err.name,
-//       data: err,
-//     });
-//     return;
-//   }
-
-//   res.status(500).json({
-//     status: 500,
-//     message: 'Something went wrong',
-//     data: err.message,
-//   });
-// };
-
 export const errorHandler = (err, req, res, next) => {
   const error = createHttpError(
     err.status || 500,
